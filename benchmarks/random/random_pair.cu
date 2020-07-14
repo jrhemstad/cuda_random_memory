ba2581f62c84c7842af3d0b855c383bf0da54832
Jake Hemstad <jhemstad@nvidia.com>
2020-07-14 12:38:42 -0500

    Add start of benchmark for loading pairs of values.

---
+++ benchmarks/random/random_pair.cu
@@ -0,0 +1,142 @@
+
+#include <benchmark/benchmark.h>
+
+#include <cuda/atomic>
+#include <synchronization.hpp>
+#include <sys/types.h>
+#include <thrust/device_vector.h>
+#include <thrust/for_each.h>
+#include <thrust/pair.h>
+#include <thrust/random.h>
+
+template <typename First, typename Second>
+void BM_weak_sequential_load(benchmark::State &state) {
+  thrust::device_vector<thrust::pair<First, Second>> input(state.range(0));
+
+  auto const begin = thrust::make_counting_iterator<int32_t>(0);
+  auto const end = thrust::make_counting_iterator<int32_t>(input.size());
+
+  for (auto _ : state) {
+    cuda_event_timer raii{state};
+    thrust::for_each(thrust::device, begin, end,
+                     [input_data = input.data().get()] __device__(auto index) {
+                       // volatile auto first = input_data[index].first;
+                       // volatile auto second = input_data[index].second;
+                     });
+  }
+  state.SetBytesProcessed(int64_t(state.iterations()) *
+                          int64_t(state.range(0)) *
+                          sizeof(thrust::pair<First, Second>));
+}
+BENCHMARK_TEMPLATE(BM_weak_sequential_load, int32_t, int32_t)
+    ->RangeMultiplier(10)
+    ->Range(100'000, 1'000'000'000)
+    ->UseManualTime()
+    ->Unit(benchmark::kMillisecond);
+/*
+
+template <typename T> void BM_weak_random_load(benchmark::State &state) {
+thrust::device_vector<T> v(state.range(0));
+
+auto l = [input_size = v.size()] __device__(auto i) {
+thrust::default_random_engine rng;
+thrust::uniform_int_distribution<int32_t> dist(0, input_size);
+rng.discard(i);
+return dist(rng);
+};
+
+auto const begin = thrust::make_transform_iterator(
+  thrust::make_counting_iterator<int32_t>(0), l);
+
+auto const end = thrust::make_transform_iterator(
+  thrust::make_counting_iterator<int32_t>(v.size()), l);
+
+for (auto _ : state) {
+cuda_event_timer raii{state};
+thrust::for_each(thrust::device, begin, end,
+                 [input_data = v.data().get()] __device__(auto index) {
+                   volatile auto l = input_data[index];
+                 });
+}
+state.SetBytesProcessed(int64_t(state.iterations()) *
+                      int64_t(state.range(0)) * sizeof(T));
+}
+BENCHMARK_TEMPLATE(BM_weak_random_load, int32_t)
+->RangeMultiplier(10)
+->Range(100'000, 1'000'000'000)
+->UseManualTime()
+->Unit(benchmark::kMillisecond);
+
+template <typename T, cuda::std::memory_order mem_order>
+void BM_atomic_sequential_load(benchmark::State &state) {
+thrust::device_vector<cuda::atomic<T, cuda::thread_scope_device>> v(
+  state.range(0));
+for (auto _ : state) {
+cuda_event_timer raii{state};
+auto const begin = thrust::make_counting_iterator(0);
+auto const end = begin + state.range(0);
+thrust::for_each(thrust::device, begin, end,
+                 [input_data = v.data().get(),
+                  input_size = v.size()] __device__(auto index) {
+                   volatile auto l = input_data[index].load(mem_order);
+                 });
+}
+state.SetBytesProcessed(int64_t(state.iterations()) *
+                      int64_t(state.range(0)) * sizeof(T));
+}
+BENCHMARK_TEMPLATE(BM_atomic_sequential_load, int32_t,
+               cuda::std::memory_order_relaxed)
+->RangeMultiplier(10)
+->Range(100'000, 1'000'000'000)
+->UseManualTime()
+->Unit(benchmark::kMillisecond);
+
+BENCHMARK_TEMPLATE(BM_atomic_sequential_load, int32_t,
+               cuda::std::memory_order_seq_cst)
+->RangeMultiplier(10)
+->Range(100'000, 1'000'000'000)
+->UseManualTime()
+->Unit(benchmark::kMillisecond);
+
+template <typename T, cuda::std::memory_order mem_order>
+void BM_atomic_random_load(benchmark::State &state) {
+thrust::device_vector<cuda::atomic<T, cuda::thread_scope_device>> v(
+  state.range(0));
+
+auto l = [input_size = v.size()] __device__(auto i) {
+thrust::default_random_engine rng;
+thrust::uniform_int_distribution<int32_t> dist(0, input_size);
+rng.discard(i);
+return dist(rng);
+};
+
+auto const begin = thrust::make_transform_iterator(
+  thrust::make_counting_iterator<int32_t>(0), l);
+
+auto const end = thrust::make_transform_iterator(
+  thrust::make_counting_iterator<int32_t>(v.size()), l);
+
+for (auto _ : state) {
+cuda_event_timer raii{state};
+thrust::for_each(thrust::device, begin, end,
+                 [input_data = v.data().get()] __device__(auto index) {
+                   volatile auto l = input_data[index].load(mem_order);
+                 });
+}
+state.SetBytesProcessed(int64_t(state.iterations()) *
+                      int64_t(state.range(0)) * sizeof(T));
+}
+BENCHMARK_TEMPLATE(BM_atomic_random_load, int32_t,
+               cuda::std::memory_order_relaxed)
+->RangeMultiplier(10)
+->Range(100'000, 1'000'000'000)
+->UseManualTime()
+->Unit(benchmark::kMillisecond);
+
+BENCHMARK_TEMPLATE(BM_atomic_random_load, int32_t,
+               cuda::std::memory_order_seq_cst)
+->RangeMultiplier(10)
+->Range(100'000, 1'000'000'000)
+->UseManualTime()
+->Unit(benchmark::kMillisecond);
+*/
